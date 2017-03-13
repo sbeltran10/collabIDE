79ad3541dd1724370d4025f2358ca9cd8496bba9
sbeltran10 <you@example.com>
2017-03-13 13:48:18 -0500

    All clients now obtain the rules for text highlighthing on login

--- public/javascripts/firepad-main.js
+++ public/javascripts/firepad-main.js
@@ -10,11 +10,12 @@ function init() {
 
     // Set user text highlighthing
     var styleSheet = document.styleSheets[document.styleSheets.length - 1];
-    styleSheet.insertRule(".firepad-username-" + collSession.username + " { background-color:"+ collSession.usercolor + "}", 0);
-    console.log(styleSheet);
+    var rules = loggedUser.rules;
+    rules.forEach(function (element) {
+        styleSheet.insertRule(element, 0);
+    });
 
     //// Initialize Firebase.
-    console.log(loggedUser);
     var config = {
         apiKey: "AIzaSyBNqso86bEnzBIzRx4RNQ-3Ww2qNqL_xJA",
         authDomain: "collabide-c9fb9.firebaseapp.com",
@@ -23,7 +24,7 @@ function init() {
     firebase.initializeApp(config);
 
     //// Get Firebase Database reference.
-    var firepadRef = getExampleRef();
+    var firepadRef = getRef();
 
     //// Create CodeMirror (with lineWrapping on).
     var codeMirror = CodeMirror(document.getElementById('firepad'), {
@@ -31,35 +32,17 @@ function init() {
         mode: 'javascript'
     });
 
-    /* Create a random ID to use as our user ID (we must give this to firepad and FirepadUserList).
-    var userId = Math.floor(Math.random() * 9999999999).toString();
-
-    var min = Math.ceil(10000);
-    var max = Math.floor(1);
-    var rand = Math.floor(Math.random() * (max - min + 1)) + min;
-    */
-
     //// Create Firepad.
     var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
-        defaultText: '// JavaScript Editing with Firepad!\nfunction go() {\n  var message = "Hello, world.";\n  console.log(message);\n}',
         userColor: collSession.usercolor,
         userId: collSession.username
     });
-
-    /* Create FirepadUserList (with our desired userId).
-    var firepadUserList = FirepadUserList.fromDiv(firepadRef.child('users'),
-        document.getElementById('userlist'), userId);
-    */
-
 }
 
-// Helper to get hash from end of URL or generate a random one.
-function getExampleRef() {
+// Helper to get databse reference.
+function getRef() {
     var ref = firebase.database().ref();
     var proID = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
     ref = ref.child(proID);
-    if (typeof console !== 'undefined') {
-        console.log('Firebase data: ', ref.toString());
-    }
     return ref;
 }
--- routes/project.js
+++ routes/project.js
@@ -14,19 +14,30 @@ firebase.initializeApp(config);
 var db = firebase.database();
 
 // Get project and user data
-function getProject(userEmail, userPassword, projectId) {
+function getProject(sessionUser, res, projectId) {
+  var rulesArray = [];
+  db.ref(projectId + "/users").once('value').then(function (snapshot) {
+    var developers = snapshot.val();
+    for (var property in developers) {
+      if (developers.hasOwnProperty(property)) {
+        var rule = ".firepad-username-" + property + " { background-color:" + developers[property].highlightColor + "}";
+        rulesArray.push(rule);
+      }
+    }
+    res.render("project", {
+      loggedUser: {
+        color: sessionUser.color,
+        username: sessionUser.name,
+        rules:rulesArray
+      }
+    });
+  });
 }
 
 
-/* GET users listing. */
 router.get('/:id', function (req, res, next) {
   var sessionUser = req.session.user;
-  res.render("project", {
-    loggedUser: {
-      color: sessionUser.color,
-      username: sessionUser.name
-    }
-  });
+  getProject(sessionUser, res, req.params.id);
 });
 
 module.exports = router;
