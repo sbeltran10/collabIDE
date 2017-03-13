7d429e7a9ec38e1621ef1d30649b4a4e965d320a
Name <s.beltran10@uniandes.edu.co>
2017-03-13 14:22:10 -0500

    User authentication almost done

--- routes/project.js
+++ routes/project.js
@@ -2,15 +2,6 @@ var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
 
-// Firebase init
-/*
-var config = {
-  apiKey: "AIzaSyBNqso86bEnzBIzRx4RNQ-3Ww2qNqL_xJA",
-  authDomain: "collabide-c9fb9.firebaseapp.com",
-  databaseURL: "https://collabide-c9fb9.firebaseio.com/"
-};
-firebase.initializeApp(config);
-*/
 var db = firebase.database();
 
 // Get project and user data
